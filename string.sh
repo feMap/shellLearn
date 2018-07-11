b070099370b6c4daa2ef2c418d0ca36d6f6fbf92
femap <296285382@qq.com>
2018-07-11 20:35:21 +0800

    Update

--- string.sh
+++ string.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 your_name="qinjx"
+# shell中的字符串可以直接拼接,也可以放在字符串中间
 greeting="hello,"$your_name"!"
 greeting_1="hello,${your_name}!"
 
+# echo 是自带换行符的
 echo $greeting $greeting_1
 
 string="abcd"
